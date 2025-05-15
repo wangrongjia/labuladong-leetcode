396647b04d402b80c899c212418905b01479a856
wangrongjia <2738890457@qq.com>
2025-05-15 12:16:29 +0800

    add

--- src/main/java/com/codinger/leetcode/C2021/June/C08/ClimbStairs.java
+++ src/main/java/com/codinger/leetcode/C2021/June/C08/ClimbStairs.java
@@ -1,86 +1,49 @@
-package com.codinger.leetcode.C2021.June.C08;
 
+//leetcode submit region begin(Prohibit modification and deletion)
 /**
- * Created on 2021/06/08.
- *
- * @author wangrongjia
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
 
-public class ClimbStairs {
+        ListNode smallTail = null;
+        ListNode smallHead = null;
+        ListNode largeHead = null;
+        do(smallTail, smallHead, largeHead, head, x);
 
-    public static void main(String[] args) {
-        int n = 45;
-
-        /**
-         * 		运行失败:
-         * 		Time Limit Exceeded
-         * 		测试用例:45
-         * 		stdout:
-         */
-//        System.out.println(dp(n));
-
-
-        /**
-         * 		解答成功:
-         * 		执行耗时:0 ms,击败了100.00% 的Java用户
-         * 		内存消耗:35.2 MB,击败了39.11% 的Java用户
-         */
-//        int[] temp = new int[n];
-//        for (int i = 1; i <= n; i++) {
-//            dp1(i, temp);
-//        }
-//        System.out.println(temp[n - 1]);
-
-
-        /**
-         *
-         * 			执行耗时:0 ms,击败了100.00% 的Java用户
-         * 			内存消耗:35 MB,击败了85.06% 的Java用户
-         */
-        int[] cache = new int[2];
-        for (int i = 1; i <= n; i++) {
-            dp2(i, cache);
-        }
-        System.out.println(cache[(n - 1) % 2]);
+        smallTail.next = largeHead;
+        return smallHead;
     }
 
-    public static int dp(int i) {
-        switch(i) {
-            case 1:
-                return 1;
-            case 2:
-                return 2;
-            default:
-                return dp(i - 1) + dp( i - 2);
+    public void do(ListNode smallTail, ListNode smallHead , ListNode largeHead, ListNode curr, int x) {
+        if (cuur == null) {
+            return;
         }
-    }
-
-
-    public static void dp1(int i, int[] temp) {
-        switch(i) {
-            case 1:
-                temp[0] = 1;
-                break;
-            case 2:
-                temp[1] = 2;
-                break;
-            default:
-                temp[i - 1] = temp[i - 2] + temp [i - 3];
-                break;
+        if (curr.val < x) {
+            // 小节点
+            if (samllHead == null) {
+                smallHead = curr;
+                smallTail = smallHead;
+            } else {
+                smallTail.next = curr;
+                smallTail = curr;
+            }
+        } else {
+            if (largeHead == null) {
+                largeHead = curr;
+            } else {
+                largeHead.next = curr;
+            }
         }
-    }
 
-    public static void dp2(int i, int[] temp) {
-        switch(i) {
-            case 1:
-                temp[0] = 1;
-                break;
-            case 2:
-                temp[1] = 2;
-                break;
-            default:
-                temp[(i - 1) % 2] = temp[(i - 2) % 2] + temp [(i - 3) % 2];
-                break;
-        }
+        do(smallTail, smallHead, largeHead, curr.next, x);
     }
 }
+//leetcode submit region end(Prohibit modification and deletion)
